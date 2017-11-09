5c4498d3fdab03b151a6458e30dabf0a2c953cc3
MallocXW <1392391958@qq.com>
2017-11-09 10:25:15 +0800

    Initial commit

---
+++ src/main/java/com/mallocxw/webauthority/WebAuthorityApplication.java
@@ -0,0 +1,12 @@
+package com.mallocxw.webauthority;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAuthorityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAuthorityApplication.class, args);
+    }
+}
